9f4f280974f8cd87f01171802e7d16f080ad2efa
Palash Mondal <palashm@live.in>
2020-12-08 11:47:21 +0530

    feat: fixed header and footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 
+  setPlatformInfo();
+  closeKeyboardOnBlur();
+
+  var inputBox = document.querySelector(".safari textarea");
+  if (inputBox) {
+    inputBox.addEventListener("focus", function (e) {
+      document.body.classList.add("keyboard");
+      setTimeout(function () {
+        window.scrollTo(0, 0);
+      }, 200);
+    });
+
+    inputBox.addEventListener("blur", function (e) {
+      document.body.classList.remove("keyboard");
+    });
+  }
+});
+
+function closeKeyboardOnBlur() {
   let cacheInput = null;
   let timer = null;
   if (!isApple()) {
@@ -8,18 +27,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }
 
   const textarea = document.querySelector("textarea");
-  const myContainer = document.querySelector(".container");
   textarea.addEventListener("focus", (event) => {
     let target = event.target;
     if (!target) return;
     cacheInput = event.target;
-
-    var displayHeight = window.innerHeight - myContainer.scrollTop;
-    myContainer.style.height = displayHeight + "px";
-  });
-
-  textarea.addEventListener("blur", (event) => {
-    myContainer.style.height = "100%";
   });
 
   document.addEventListener("touchend", function (event) {
@@ -33,12 +44,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
       }, 300);
     }
   });
+}
 
-  function isApple() {
-    let ua = navigator.userAgent.toUpperCase();
-    let ipad = ua.indexOf("IPAD") > -1,
-      ipod = ua.indexOf("IPOD") > -1,
-      iphone = ua.indexOf("IPHONE") > -1;
-    return ipad || ipod || iphone;
+function setPlatformInfo() {
+  var ua = navigator.userAgent.toLowerCase();
+  if (ua.indexOf("safari") != -1) {
+    if (ua.indexOf("chrome") > -1) {
+      document.body.classList.add("chrome");
+    } else {
+      document.body.classList.add("safari");
+    }
   }
-});
+}
+
+function isApple() {
+  let ua = navigator.userAgent.toUpperCase();
+  let ipad = ua.indexOf("IPAD") > -1,
+    ipod = ua.indexOf("IPOD") > -1,
+    iphone = ua.indexOf("IPHONE") > -1;
+  return ipad || ipod || iphone;
+}
